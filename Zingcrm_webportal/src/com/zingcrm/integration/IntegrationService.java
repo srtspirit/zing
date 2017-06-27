6baa5c622ded42a0b1b2292ba509e699461e413b
Andrey <Andrey@Spirit-Notebook>
2017-06-27 06:41:04 -0400

    integration module

--- Zingcrm_webportal/src/com/zingcrm/integration/IntegrationService.java
+++ Zingcrm_webportal/src/com/zingcrm/integration/IntegrationService.java
@@ -1,7 +1,9 @@
 package com.zingcrm.integration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.zingcrm.forms.LeadForms;
 import com.zingcrm.jms.JmsProducer;
@@ -10,10 +12,21 @@ import com.zingcrm.jms.JmsProducer;
 public class IntegrationService {
 	@Autowired
 	private JmsProducer jmsProducer;
+	@Autowired
+	private RestTemplate restTemplate;
+	@Value("${bus.rest.upgradebp.url}")
+	private String esbUrlUpdateBp;
 	
+
 	public void updateBPIntegration(LeadForms lead){
-		jmsProducer.sendObjectMessage("DELAYED", lead);
+		try {
+			restTemplate.postForObject(esbUrlUpdateBp, lead, String.class);
+		} catch (Throwable e) {
+			e.printStackTrace();
+			jmsProducer.sendObjectMessage("DELAYED", lead);
+		}	
 	}
+	
 
 	public JmsProducer getJmsProducer() {
 		return jmsProducer;
@@ -21,5 +34,21 @@ public class IntegrationService {
 
 	public void setJmsProducer(JmsProducer jmsProducer) {
 		this.jmsProducer = jmsProducer;
+	}
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+
+	public void setRestTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public String getEsbUrlUpdateBp() {
+		return esbUrlUpdateBp;
+	}
+
+
+	public void setEsbUrlUpdateBp(String esbUrlUpdateBp) {
+		this.esbUrlUpdateBp = esbUrlUpdateBp;
 	}
 }
